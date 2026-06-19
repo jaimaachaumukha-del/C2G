81da38aeb2bf59e7b63de3aa3cea03a046764833
aditya-sharma121 <jaimaachaumukha@gmail.com>
2026-06-19 19:31:23 +0530

    [C2G Practice] Solved: Two Sum

---
+++ practice/medium/two-sum/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    System.out.print(i + " " + j);
+                    return;
+                }
+            }
+        }
+    }
+}
